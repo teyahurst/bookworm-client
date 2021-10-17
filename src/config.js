1ff5cea9b5fcb15c7071b665ee7cf11f9173e492
Teya Hurst <teyahurst@gmail.com>
2021-10-17 06:26:01 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const config = {
-    API_ENDPOINT: process.env.REACT_APP_BASE_URL,
+    API_ENDPOINT: process.env.API_ENDPOINT,
     REACT_APP_API_KEY: process.env.REACT_APP_API_KEY,
     TOKEN_KEY: 'bookworm-client-auth-token',
     
