8d46996623bc5dacfbf1fa17db2b2cd33d0f5287
Teya Hurst <teyahurst@gmail.com>
2021-10-20 17:25:33 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const config = {
-    API_ENDPOINT: 'http://localhost:8000',
+    API_ENDPOINT: process.env.API_ENDPOINT,
     REACT_APP_API_KEY: process.env.REACT_APP_API_KEY,
     TOKEN_KEY: process.env.TOKEN_KEY,
     
