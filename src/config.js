3e3788edfb833d8565f5eab4988637f3eb8fa94f
Teya Hurst <teyahurst@gmail.com>
2021-10-20 17:22:10 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 
 const config = {
-    API_ENDPOINT: 'https://sleepy-reef-12097.herokuapp.com',
+    API_ENDPOINT: 'http://localhost:8000',
     REACT_APP_API_KEY: process.env.REACT_APP_API_KEY,
-    TOKEN_KEY: 'bookworm-client-auth-token',
+    TOKEN_KEY: process.env.TOKEN_KEY,
     
 }
 
