d80074761aced5a76e356356ff5d8046d96ea664
Teya Hurst <teyahurst@gmail.com>
2021-10-20 17:42:25 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const config = {
-    API_ENDPOINT: process.env.API_ENDPOINT,
+    API_ENDPOINT: 'https://sleepy-reef-12097.herokuapp.com',
     REACT_APP_API_KEY: process.env.REACT_APP_API_KEY,
     TOKEN_KEY: process.env.TOKEN_KEY,
     
